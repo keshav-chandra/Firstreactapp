4e2b40e61855e34bbf4b13c3063af40cda8ca37f
keshav-chandra <keshavchandra2222@gmail.com>
2023-08-21 23:24:55 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+let name="keshav";
+function App() {
+  return (
+   <>
+<nav>
+  <li>Home </li>
+  <li>About</li>
+  <li>contact</li>
+</nav>
+<div className="container">
+  <h1>Hello {name}</h1>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quae dignissimos repudiandae cum esse quisquam, nostrum consectetur quia libero, sapiente debitis assumenda in temporibus? Esse quae nulla debitis soluta dolorem!
+</div>
+</>
+  );
+}
+
+export default App;
+
